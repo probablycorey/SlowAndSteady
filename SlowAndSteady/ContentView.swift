32e27ce58e8079d3bf4ad389375d303f13f7c63a
Corey Johnson <probablycorey@gmail.com>
2021-07-01 14:14:25 -0700

    Initial Commit

---
+++ SlowAndSteady/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlowAndSteady
+//
+//  Created by Corey Johnson on 7/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlowAndSteady/SlowAndSteadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlowAndSteadyApp.swift
+//  SlowAndSteady
+//
+//  Created by Corey Johnson on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SlowAndSteadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
